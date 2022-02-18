401bc374079abe88e9d3e7d070edfdc09c6a5786
runninggator <runninggator94@gmail.com>
2022-02-18 09:39:51 -0500

    fixed test datasource bug in dataloader

--- data/MRI2CT_dataset.py
+++ data/MRI2CT_dataset.py
@@ -13,7 +13,8 @@ class MRI2CTDataset(BaseDataset):
     def initialize(self, opt):
         self.opt = opt
         self.root = opt.dataroot
-        self.samples = os.listdir(os.path.join(opt.dataroot, 'A/train'))
+        self.type = 'train' if opt.isTrain else 'test'
+        self.samples = os.listdir(os.path.join(opt.dataroot, f'A/{self.type}'))
 
         random.shuffle(self.samples)
 
@@ -21,8 +22,8 @@ class MRI2CTDataset(BaseDataset):
         sample = self.samples[index]
 
         # load t2 and ct scans
-        t2_scan = np.load(os.path.join(self.root, 'A/train/', sample))
-        ct_scan = np.load(os.path.join(self.root, 'B/train/', sample))
+        t2_scan = np.load(os.path.join(self.root, f'A/{self.type}/', sample))
+        ct_scan = np.load(os.path.join(self.root, f'B/{self.type}/', sample))
 
         # convert to torch tensors with dimension [channel, z, x, y]
         # t2_scan = torch.from_numpy(t1_scan[None, ])
