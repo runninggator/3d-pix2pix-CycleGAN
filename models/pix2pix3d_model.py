2317d8ca4524c548c27ad0f71047add60e3a4f32
runninggator <runninggator94@gmail.com>
2022-07-24 20:15:27 -0400

    Change test set_input function

--- models/pix2pix3d_model.py
+++ models/pix2pix3d_model.py
@@ -56,8 +56,13 @@ class Pix2Pix3dModel(BaseModel):
 
     def set_input(self, input):
         AtoB = self.opt.which_direction == 'AtoB'
-        input_A = input['A' if AtoB else 'B']
-        input_B = input['B' if AtoB else 'A']
+        if self.isTrain:
+            input_A = input['A' if AtoB else 'B']
+            input_B = input['B' if AtoB else 'A']
+        else:
+            input_A = input[0 if AtoB else 1]
+            input_B = input[1 if AtoB else 0]
+        
         self.input_A.resize_(input_A.size()).copy_(input_A)
         self.input_B.resize_(input_B.size()).copy_(input_B)
         #self.image_paths = input['A_paths' if AtoB else 'B_paths']
